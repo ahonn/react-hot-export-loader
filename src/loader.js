e331ea9df82e7e4aa215f043a6ddc38aba9b4011
ahonn <ahonn95@outlook.com>
2019-07-15 10:53:47 +0800

    feat: add options.identifier & options.filter

--- src/loader.js
+++ src/loader.js
@@ -2,18 +2,10 @@ const { parse } = require('@babel/parser');
 const generate = require('@babel/generator').default;
 const traverse = require('@babel/traverse').default;
 const t = require('@babel/types');
+const loaderUtils = require('loader-utils');
 
-const reactHotLoaderIdentfierName = 'hot';
+const DEFAULT_IDENTIFIER = '__HOT__';
 const parseOptions = { sourceType: 'module', plugins: ['jsx'] };
-
-const importReactHotLoaderDeclaration = t.importDeclaration(
-  [t.importSpecifier(t.identifier(reactHotLoaderIdentfierName), t.identifier('hot'))],
-  t.stringLiteral('react-hot-loader')
-);
-
-function callHotModuleExpression(args) {
-  return t.callExpression(t.callExpression(t.identifier('hot'), [t.identifier('module')]), args);
-}
 
 function isModuleExport(node) {
   if (t.isMemberExpression(node)) {
@@ -28,27 +20,46 @@ function isModuleExport(node) {
 }
 
 function ReactHotExportLoader(source) {
-  if (process.env.NODE_ENV === 'production' || source === '') {
+  const options = loaderUtils.getOptions(this) || {};
+  const { identifier = DEFAULT_IDENTIFIER, filter } = options;
+
+  const isProductionEnv = () => process.env.NODE_ENV === 'production';
+  const shouldSkip = () => typeof filter === 'function' && !filter(this);
+
+  if (!source || isProductionEnv() || shouldSkip()) {
     return source;
   }
 
   const ast = parse(source, parseOptions);
+
+  // insert `import { hot as ${identifier} } from 'react-hot-loader';`
+  const importReactHotLoaderDeclaration = t.importDeclaration(
+    [t.importSpecifier(t.identifier(identifier), t.identifier('hot'))],
+    t.stringLiteral('react-hot-loader')
+  );
   ast.program.body.unshift(importReactHotLoaderDeclaration);
 
   traverse(ast, {
+    // change `export default xxx` to `export default ${identifier}(module)(xxx)`
     ExportDefaultDeclaration: (path) => {
-      path.node.declaration = callHotModuleExpression([path.node.declaration]);
+      path.node.declaration = t.callExpression(
+        t.callExpression(t.identifier(identifier), [t.identifier('module')]),
+        [path.node.declaration]
+      );
     },
+    // change `module.exports = xxx` to `module.exports = ${identifier}(module)(xxx)`
     ExpressionStatement: (path) => {
       const { expression } = path.node;
       if (expression.operator === '=' && isModuleExport(expression.left)) {
-        path.node.expression.right = callHotModuleExpression([path.node.expression.right]);
+        path.node.expression.right = t.callExpression(
+          t.callExpression(t.identifier(identifier), [t.identifier('module')]),
+          [path.node.expression.right]
+        );
       }
     },
   });
 
   const { code } = generate(ast);
-
   return code;
 }
 
